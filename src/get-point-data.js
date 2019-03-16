63a630b0b9f5cf11f327fd7caea482038b96a38b
Morfey216 <Morfey4web@gmail.com>
2019-03-16 16:24:23 +0300

    Adding data generation functions

---
+++ src/get-point-data.js
@@ -0,0 +1,110 @@
+import {
+  getNumberFromRange,
+  getItemsFromArray} from './random.js';
+
+const TIME_INTERVAL = {
+  MIN_HOUR: 0,
+  MAX_HOUR: 23,
+  MIN_MINUTE: 0,
+  MAX_MINUTE: 59
+};
+
+const OFFERS_INTERVAL = {
+  MIN: 0,
+  MAX: 2
+};
+
+const DESCRIPTIONS_INTERVAL = {
+  MIN: 1,
+  MAX: 3
+};
+
+const TYPES = {
+  'Taxi': `🚕`,
+  'Bus': `🚌`,
+  'Train': `🚂`,
+  'Ship': `🛳️`,
+  'Transport': `🚊`,
+  'Drive': `🚗`,
+  'Flight': `✈️`,
+  'Check-in': `🏨`,
+  'Sightseeing': `🏛️`,
+  'Restaurant': `🍴`
+};
+
+const DESCRIPTIONS = [
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+  `Cras aliquet varius magna, non porta ligula feugiat eget.`,
+  `Fusce tristique felis at fermentum pharetra.`,
+  `Aliquam id orci ut lectus varius viverra.`,
+  `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
+  `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
+  `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
+  `Sed sed nisi sed augue convallis suscipit in sed felis.`,
+  `Aliquam erat volutpat.`,
+  `Nunc fermentum tortor ac porta dapibus.`,
+  `In rutrum ac purus sit amet tempus.`
+];
+
+const OFFERS = [
+  `Add luggage`,
+  `Switch to comfort class`,
+  `Add meal`,
+  `Choose seats`
+];
+
+const createSchedule = () => {
+  const start = Date.now() + getNumberFromRange(TIME_INTERVAL.MIN_HOUR, TIME_INTERVAL.MAX_HOUR) * 60 * 60 * 1000;
+  const end = start + (getNumberFromRange(TIME_INTERVAL.MIN_HOUR, TIME_INTERVAL.MAX_HOUR) * 60 + getNumberFromRange(TIME_INTERVAL.MIN_MINUTE, TIME_INTERVAL.MAX_MINUTE)) * 60 * 1000;
+
+  const options = {
+    hour12: false,
+    hour: `numeric`,
+    minute: `numeric`
+  };
+
+  return ({
+    // startTime: start.getHours(),
+    // startTime: start.toLocaleString(`en-US`, options),
+    // endTime: end.toLocaleString(`en-US`, options),
+    startTime: Intl.DateTimeFormat(`en-US`, options).format(start),
+    endTime: Intl.DateTimeFormat(`en-US`, options).format(end),
+    duration: Intl.DateTimeFormat(`en-US`, options).format(end - start)
+    // duration: (end - start).toLocaleString(`en-US`, options)
+  });
+};
+
+const createOffers = () => {
+  const offersName = getItemsFromArray(OFFERS.slice(), getNumberFromRange(OFFERS_INTERVAL.MIN, OFFERS_INTERVAL.MAX + 1));
+  const offers = [];
+  for (const offer of offersName) {
+    const currentOffer = {
+      name: offer,
+      price: getNumberFromRange()
+    };
+
+    offers.push(currentOffer);
+  }
+
+  return offers;
+};
+
+const createDescription = () => getItemsFromArray(DESCRIPTIONS.slice(), getNumberFromRange(DESCRIPTIONS_INTERVAL.MIN, DESCRIPTIONS_INTERVAL.MAX + 1)).join(` `);
+
+const createCurrentType = () => {
+  const typeNames = Object.keys(TYPES);
+  const type = typeNames[getNumberFromRange(0, typeNames.length - 1)];
+  return ({
+    title: type,
+    icon: TYPES[type]
+  });
+};
+
+export default () => ({
+  type: createCurrentType(),
+  schedule: createSchedule(),
+  price: getNumberFromRange(),
+  offers: createOffers(),
+  description: createDescription()
+
+});
---
+++ src/random.js
@@ -0,0 +1,12 @@
+export const getNumberFromRange = (minNumber = 1, maxNumber = 100) => Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber;
+
+export const getItemsFromArray = (array, number) => {
+  const resultArray = [];
+
+  for (let i = 0; i < number; i++) {
+    resultArray.push(array.splice(getNumberFromRange(0, array.length), 1)[0]);
+    console.log(number);
+  }
+
+  return resultArray;
+};
