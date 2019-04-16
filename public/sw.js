d2e78ce9f95ffb6f300de250ff53ac3ecd08d574
Morfey216 <Morfey4web@gmail.com>
2019-04-16 10:16:43 +0300

    added ServiceWorker

---
+++ public/sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = `STATIC_V1.0`;
+
+self.addEventListener(`install`, (evt) => {
+  evt.waitUntil(caches.open(CACHE_NAME)
+    .then((cache) => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+        `/css/normalize.css`,
+        `/css/main.css`,
+        `/img/star.svg`,
+        `/img/star--check.svg`,
+        `/bundle.js`,
+      ]);
+    })
+    .catch((err) => {
+      throw err;
+    })
+  );
+});
+
+const refreshCache = (request) => {
+  return fetch(request)
+    .then((response) => {
+      caches.open(CACHE_NAME)
+        .then((cache) => cache.put(request, response.clone()));
+      return response.clone();
+    });
+};
+
+self.addEventListener(`fetch`, (evt) => {
+  evt.respondWith(caches.match(evt.request)
+    .then((response) => {
+      return response ? response : refreshCache(evt.request);
+    })
+    .catch((err) => {
+      throw err;
+    })
+  );
+});
